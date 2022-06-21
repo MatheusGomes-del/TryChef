3321be9bafbd3ff1e35268fc2a4140cca6c5282f
MatheusGomes-del <matheus.gomes.santos@outlook.com>
2022-06-21 18:28:29 -0300

    feat: adiciona botões de filtro em food e drink

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -96,6 +96,7 @@ export default function AppProvider({ children }) {
         return error;
       }
     }
+
     getFoods();
     getDrinks();
   }, []);
--- src/pages/Food.js
+++ src/pages/Food.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import AppContext from '../context/AppContext';
 import FoodCard from './components/FoodCard';
 import Header from './components/Header';
@@ -7,12 +7,40 @@ import MenuBar from './components/MenuBar';
 export default function Food() {
   const { list } = useContext(AppContext);
   const { foods } = useContext(AppContext);
-  console.log(foods);
+  const [categoryFood, setCategoryFood] = useState([]);
+  const [magigNumber] = useState('5');
+
+  useEffect(() => {
+    async function getCategorysFood() {
+      try {
+        const endopint = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+        const response = await fetch(endopint);
+        const { meals } = await response.json();
+        setCategoryFood(meals);
+      } catch (error) {
+        return error;
+      }
+    }
+
+    getCategorysFood();
+  }, []);
 
   return (
     <>
       <Header title="Foods" enableSearchButton />
       <div>
+        <section>
+          { categoryFood.map((category, index) => (
+            <button
+              data-testid={ `${category.strCategory}-category-filter` }
+              type="button"
+              key={ index }
+            >
+              { category.strCategory }
+
+            </button>
+          )).slice(0, Number(magigNumber)) }
+        </section>
         {
           foods.map((item, i) => (
             <FoodCard key={ item.idMeal } food={ item } idTest={ i } />
