dd1436a32555a0de64075d4853c9912bafcd100c
MatheusGomes-del <matheus.gomes.santos@outlook.com>
2022-06-22 22:32:27 -0300

    feat: adiciona botões e renderiza email, OBS: 82,83,84 estão passando juntos

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import MenuBar from './components/MenuBar';
 
 export default function Profile() {
+  const [emailUser, setEmail] = useState('');
+
+  useEffect(() => {
+    function getEmailFromLocalStorage() {
+      const { email } = JSON.parse(localStorage.getItem('user'));
+      setEmail(email);
+      console.log(email);
+    }
+
+    getEmailFromLocalStorage();
+  }, []);
+
   return (
     <>
       <Header title="Profile" />
-      <div>Profile</div>
+      <div>
+        <section>
+          <p data-testid="profile-email">{ emailUser }</p>
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+            className="buttons"
+          >
+            Done Recipes
+
+          </button>
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Favorite Recipes
+
+          </button>
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+          >
+            Logout
+
+          </button>
+        </section>
+      </div>
       <MenuBar />
     </>
   );
