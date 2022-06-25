7ef1988552593310e3e109345e47f564a6238c7a
Rodrigo Takashi Condo <rodrigo0takashi@gmail.com>
2022-06-25 11:54:05 -0300

    feat(done recipes): finaliza tela de receitas feitas

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import DoneRecipeCard from './components/DoneRecipeCard';
 import Header from './components/Header';
 
 export default function DoneRecipes() {
@@ -6,55 +7,65 @@ export default function DoneRecipes() {
 
   useEffect(() => {
     function getRecipesDone() {
-      const recipes = JSON.parse(localStorage.getItem('doneRecipes'));
+      const recipes = JSON.parse(localStorage.getItem('doneRecipes')) || [];
       setRecipesDone(recipes);
       console.log(recipes);
     }
-
     getRecipesDone();
   }, []);
+
+  const filterFoods = () => {
+    const recipes = JSON.parse(localStorage.getItem('doneRecipes'));
+    const foodList = recipes.filter(({ type }) => type === 'food');
+    setRecipesDone(foodList);
+  };
+
+  const filterDrinks = () => {
+    const recipes = JSON.parse(localStorage.getItem('doneRecipes'));
+    const drinkList = recipes.filter(({ type }) => type === 'drink');
+    setRecipesDone(drinkList);
+  };
+
+  const returnAllDones = () => {
+    const recipes = JSON.parse(localStorage.getItem('doneRecipes'));
+    setRecipesDone(recipes);
+  };
 
   return (
     <>
       <Header title="Done Recipes" />
       <div>
         <section>
-          <button type="button" data-testid="filter-by-all-btn">All</button>
-          <button type="button" data-testid="filter-by-food-btn">Food</button>
+          <button
+            type="button"
+            data-testid="filter-by-all-btn"
+            onClick={ returnAllDones }
+          >
+            all
+          </button>
+          <button
+            type="button"
+            data-testid="filter-by-food-btn"
+            onClick={ filterFoods }
+
+          >
+            food
+          </button>
           <button
             type="button"
             data-testid="filter-by-drink-btn"
+            onClick={ filterDrinks }
           >
-            Drink
-
+            drink
           </button>
           {
-            recipesDone.map((igredient, index) => (
-              <div key={ index }>
-                <img
-                  src={ igredient.image }
-                  data-testid={ `${index}-horizontal-image` }
-                  alt="comida"
-                />
-                <p data-testid={ `${index}-horizontal-top-text` } />
-                { igredient.category }
-                <h2 data-testid={ `${index}-horizontal-name` }>{ igredient.name }</h2>
-                <p data-testid={ `${index}-horizontal-done-date` }>
-                  { igredient.doneDate }
-
-                </p>
-                <p data-testid={ `${index}-horizontal-share-btn` }>
-                  { igredient.nacionality}
-
-                </p>
-                <p data-testid={ `${index}-${igredient.tags[0]}-tag` }>
-                  { igredient.tags[0] }
-
-                </p>
-                <p data-testid={ `${index}-${igredient.tags[1]}-tag` }>
-                  { igredient.tags[1] }
-                </p>
-              </div>
+            recipesDone.map((recipe, index) => (
+              <DoneRecipeCard
+                key={ index }
+                index={ index }
+                recipe={ recipe }
+                setList={ setRecipesDone }
+              />
             ))
           }
 
--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-
 import profileIcon from '../../images/profileIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
 import BarraDeBusca from './BarraDeBusca';
